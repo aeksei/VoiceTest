eae98d15f8cda47c655f9d02511073fc335a6fde
aeksei <aeksei94@gmail.com>
2019-05-18 00:34:27 +0300

    save voice msg to database

--- db.py
+++ db.py
@@ -1,43 +1,52 @@
 import sqlite3
-from datetime import datetime
 
 DATABASE = "VoiceDB.db"
 
 
 class VoiceDB:
     def __init__(self, database):
-        self.conn = sqlite3.connect(database)  # или :memory: чтобы сохранить в RAM
-        self.cursor = self.conn.cursor()
+        self.conn = sqlite3.connect(database, check_same_thread = False)
+        self.cur = self.conn.cursor()
 
-        self.cursor.execute(
-            '''CREATE TABLE if not exists users (
-            "uid" INTEGER PRIMARY KEY, 
-            "username" TEXT)''')
+        sql = '''CREATE TABLE if not exists users (
+        "uid" INTEGER PRIMARY KEY, 
+        "username" TEXT)'''
+        self.query(sql)
+
+    def __del__(self):
+        self.conn.close()
+
+    def query(self, sql, args=[]):
+        self.cur.execute(sql, args)
         self.conn.commit()
+        return self.cur
+
+    def get_users(self):
+        return [u[0] for u in self.query("SELECT uid FROM users")]
 
     def create_voice_table(self, uid):
-        self.cursor.execute(
-            '''CREATE TABLE if not exists voices_by_{} (
-            "id"   INTEGER PRIMARY KEY AUTOINCREMENT,
-            "timestamp" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            "voice"  BLOB NOT NULL
-            );'''.format(uid))
-        self.conn.commit()
+        sql = '''CREATE TABLE if not exists voices_by_{} (
+        "id"   INTEGER PRIMARY KEY AUTOINCREMENT,
+        "timestamp" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        "voice"  BLOB NOT NULL
+        );'''.format(uid)
+        self.query(sql)
 
     def add_user(self, uid, username):
-        self.cursor.execute('INSERT INTO users VALUES(?, ?);', (uid, username))
+        self.query('INSERT INTO users VALUES(?, ?);', [uid, username])
         self.create_voice_table(uid)
-        self.conn.commit()
 
     def write_voice(self, uid, voice):
         sql = "INSERT INTO voices_by_{uid} (voice) VALUES(?);".format(uid=uid)
-        self.cursor.execute(sql, [sqlite3.Binary(voice)])
-        self.conn.commit()
+        self.query(sql, [sqlite3.Binary(voice)])
 
 
 if __name__ == "__main__":
     db = VoiceDB(DATABASE)
-    db.add_user(123, "test")
+    users = db.get_users()
+    uid = 1234
+    if uid in users:
+        db.add_user(uid, "test")
     with open("main.py", "rb") as input_file:
         ablob = input_file.read()
-        db.write_voice("123", ablob)
+        db.write_voice(1234, ablob)
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import telebot
+import db
 
 TOKEN = "681428134:AAFidZQVN2NIa_FFZlItvgHEsRjMjEQQ76I"
+DATABASE = "VoiceDB.db"
 
 if __name__ == "__main__":
     bot = telebot.TeleBot(TOKEN)
+    db = db.VoiceDB(DATABASE)
+    users = db.get_users()
 
     @bot.message_handler(commands=['start', 'help'])
     def send_welcome(message):
         bot.reply_to(message, "Howdy, how are you doing?")
 
-    @bot.message_handler(content_types=['text'])
-    def send_welcome(message):
-        bot.reply_to(message, "Howdy, how are you doing?")
+    @bot.message_handler(content_types=['voice'])
+    def handle_docs_voice(message):
+        try:
+            uid = message.chat.id
+            if uid not in users:
+                db.add_user(uid, message.chat.username)
+                users.append(uid)
+
+            file_info = bot.get_file(message.voice.file_id)
+            voice = bot.download_file(file_info.file_path)
+            db.write_voice(uid, voice)
+
+            bot.reply_to(message, "Аудио сообщение добавлено")
+        except Exception as e:
+            bot.reply_to(message, e)
 
     bot.polling(none_stop=False, interval=0, timeout=20)
